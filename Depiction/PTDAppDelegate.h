4bb9ee95b85c20f29704877a742e92be63c4c325
Will Allen <tathianamonacella@Wills-MacBook-Pro-Retina.local>
2014-03-26 10:35:33 +0000

    Initial Commit

---
+++ Depiction/PTDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PTDAppDelegate.h
+//  Depiction
+//
+//  Created by Will Allen on 26/03/2014.
+//  Copyright (c) 2014 Painted Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
